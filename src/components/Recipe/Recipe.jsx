00b383b67b83b802390095d138df5d0229456b32
sanchousser <golyukaleks@gmail.com>
2025-03-06 19:09:20 +0100

    upd

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,10 +1,24 @@
+import PropTypes from "prop-types"
 import { RecipeInfo } from "../RecipeInfo/RecipeInfo"
-
+import css from './Recipe.module.css'
 
 export const Recipe = ({ recipe }) => {
-  return (<ul>
-    {recipe.map(({name, image, time, servings, calories, difficulty}) => {
-      return <RecipeInfo name={name} imageUrl={image} time={time} servings={servings} calories={calories} difficulty={difficulty}/>
-    })}
-  </ul>)
+  return (
+    <ul className={css.recipe__list}>
+      {recipe.map(({ image, name, time, servings, calories, difficulty}, index) => {
+        return <RecipeInfo
+          key={index}
+          image={image}
+          name={name}
+          time={time}
+          servings={servings}
+          calories={calories}
+          difficulty={difficulty}
+        />
+      })}
+    </ul>
+  );
+};
+Recipe.propTypes = {
+  recipe: PropTypes.array
 }
--- src/components/RecipeInfo/RecipeInfo.jsx
+++ src/components/RecipeInfo/RecipeInfo.jsx
@@ -1,22 +1,26 @@
 import PropTypes from 'prop-types';
+import css from './RecipeInfo.module.css'
+import { IoTimeOutline } from "react-icons/io5";
+import { MdOutlineStars } from "react-icons/md";
+import { IoPeople } from "react-icons/io5";
+import { CiWheat } from "react-icons/ci";
 
-
-export const RecipeInfo = ({imageUrl, name, time, servings, calories, difficulty}) => {
+export const RecipeInfo = ({image, name, time, servings, calories, difficulty}) => {
     return (
-        <li>
-            <img src={imageUrl} width='400' alt={name} />
-            <h2>{name}</h2>
-            <p><b>Cooking time: </b>{time} minutes</p>
-            <p><b>Serving for: </b>{servings} {servings > 1 ? 'people' : 'person'}</p>
-            <p><b>Calories: </b>{calories}kcal</p>
-            <p><b>Cooking difficulty: </b>{difficulty}stars from 3</p>
+        <li  className={css.recipe__item}>
+            <img className={css.recipe__img} src={image} alt={name} />
+            <h2 className={css.recipe__title}>{name}</h2>
+            <p className={css.time}><IoTimeOutline size={25} className={css.recipe__icon} /><b>Cooking time: </b>{time} minutes</p>
+            <p className={css.serving}><IoPeople size={25} className={css.recipe__icon}/><b>Serving for: </b> {servings} {servings > 1 ? 'people' : 'person'}</p>
+            <p className={css.calories}><CiWheat size={25} className={css.recipe__icon} /><b>Calories: </b> {calories}kcal</p>
+            <p className={css.difficulty}><MdOutlineStars size={25} className={css.recipe__icon} /><b>Cooking difficulty: </b> {difficulty} stars from 3</p>
 
         </li>
     )
 }
 
 RecipeInfo.propTypes = {
-    imageUrl: PropTypes.string,
+    image: PropTypes.string,
     name: PropTypes.string,
     time: PropTypes.string,
     servings: PropTypes.number,
