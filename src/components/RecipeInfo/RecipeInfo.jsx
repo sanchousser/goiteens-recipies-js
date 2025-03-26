fb0f77782296e19aa0fcbc08f3d8e40585a9936a
sanchousser <golyukaleks@gmail.com>
2025-03-26 12:51:50 +0100

    upd

--- src/components/RecipeInfo/RecipeInfo.jsx
+++ src/components/RecipeInfo/RecipeInfo.jsx
@@ -22,6 +22,7 @@ const Image = styled.img`
 
 const Title = styled.h2`
     font-size: 22px;
+    text-align: center;
 `
 
 const DescrText = styled.p`
@@ -30,26 +31,64 @@ const DescrText = styled.p`
     gap: 5px;
     svg {
     margin-right: 8px;
+    width: 20px;
+    height: 20px;
   }
 `
 
+const DifficultyList = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    height: 35px;
+    border-radius: 15px;
+    padding-inline-start: 0px;
+
+`
+
 const Difficulty = styled.span`
-    background-color: #FF8096;
-    padding: 3px;
+    background-color: #FFB6C1;
+    padding: 4px;
     border-radius: 7px;
-    color: white;
+    color: black;
     font-weight: bold;
 `
+
+const RecipeDifficulty = styled(Difficulty)`
+    background-color: #9F2B68;
+    color: white;
+`
+
 
 export const RecipeInfo = ({ image, name, time, servings, calories, difficulty }) => {
     return (
         <Item>
             <Image src={image} alt={name} />
             <Title>{name}</Title>
-            <DescrText><IoTimeOutline  /><b>Cooking time: </b>{time} minutes</DescrText>
-            <DescrText><IoPeople  /><b>Serving for: </b> {servings} {servings > 1 ? 'people' : 'person'}</DescrText>
-            <DescrText><CiWheat  /><b>Calories: </b> {calories}kcal</DescrText>
-            <DescrText><MdOutlineStars  /><b>Cooking difficulty: </b><Difficulty>{difficulty === 1 ? 'Easy' : difficulty === 2 ? 'Medium' : 'Hard'}</Difficulty></DescrText>
+            <DescrText><IoTimeOutline /><b>Cooking time: </b>{time} minutes</DescrText>
+            <DescrText><IoPeople /><b>Serving for: </b> {servings} {servings > 1 ? 'people' : 'person'}</DescrText>
+            <DescrText><CiWheat /><b>Calories: </b> {calories}kcal</DescrText>
+            <DescrText><MdOutlineStars /><b>Cooking difficulty: </b></DescrText>
+
+            {(() => {
+                if (difficulty === 1) {
+                    return <DifficultyList>
+                        <RecipeDifficulty>Easy</RecipeDifficulty><Difficulty>Medium</Difficulty><Difficulty>Hard</Difficulty>
+                    </DifficultyList>;
+                } else if (difficulty === 2) {
+                    return <DifficultyList>
+                        <Difficulty>Easy</Difficulty><RecipeDifficulty>Medium</RecipeDifficulty><Difficulty>Hard</Difficulty>
+                    </DifficultyList>;
+                } else {
+                    return <DifficultyList>
+                        <Difficulty>Easy</Difficulty><Difficulty>Medium</Difficulty><RecipeDifficulty>Hard</RecipeDifficulty>
+                    </DifficultyList>;
+                }
+            })()}
+
+
+            {/* <Difficulty>{difficulty === 1 ? 'Easy' : difficulty === 2 ? 'Medium' : 'Hard'}</Difficulty> */}
 
         </Item>
     )
